4014e632a1335116e608012ec8e7b811647555ff
Absolutt87 <pedrodriguez87@hotmail.com>
2018-09-13 15:40:25 +0200

    Write products to JSON

--- src/main/java/tech/bts/productcatalog/ProductCatalog.java
+++ src/main/java/tech/bts/productcatalog/ProductCatalog.java
@@ -1,5 +1,7 @@
 package tech.bts.productcatalog;
 
+import com.google.gson.Gson;
+
 import java.io.PrintWriter;
 import java.util.*;
 
@@ -19,10 +21,25 @@ public class ProductCatalog {
         products.add(p2);
         products.add(p3);
 
-        writeCSV(products);
+        writeJSON(products);
 
 
         System.out.println("File written");
+    }
+
+    public static void writeJSON(List<Product> products) throws Exception {
+
+        Gson gson = new Gson();
+        String json = gson.toJson(products);
+
+        System.out.println(json);
+
+        PrintWriter writer = new PrintWriter("products.json");
+
+        writer.println(json);
+
+        writer.close();
+
     }
 
     public static void writeCSV(List<Product> products) throws Exception {
