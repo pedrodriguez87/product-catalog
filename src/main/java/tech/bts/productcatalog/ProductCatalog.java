295332e21da80fcca409f2de3d5738ab9da131c1
Absolutt87 <pedrodriguez87@hotmail.com>
2018-09-13 13:29:35 +0200

    Refactored ProductCatalog: create writeCSV method

--- src/main/java/tech/bts/productcatalog/ProductCatalog.java
+++ src/main/java/tech/bts/productcatalog/ProductCatalog.java
@@ -19,6 +19,13 @@ public class ProductCatalog {
         products.add(p2);
         products.add(p3);
 
+        writeCSV(products);
+
+
+        System.out.println("File written");
+    }
+
+    public static void writeCSV(List<Product> products) throws Exception {
 
         PrintWriter writer = new PrintWriter("products.csv");
 
@@ -37,8 +44,6 @@ public class ProductCatalog {
 
 
         writer.close();
-
-        System.out.println("File written");
     }
 }
 
